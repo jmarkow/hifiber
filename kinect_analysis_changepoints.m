c5885354ef8011c8c331f8b291275302eeae5a91
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2016-06-21 19:18:47 -0400

    New analysis

---
+++ kinect_analysis_changepoints.m
@@ -0,0 +1,43 @@
+function [RPS,SCORE,CHANGEPOINTS,THRESH]=kinect_analysis_changepoints(FEATURES,varargin)
+%
+%
+%
+%
+
+
+delta_thresh=.3;
+delta_win=8;
+smooth_sig=2;
+jl_bound_eps=.25;
+
+nparams=length(varargin);
+
+if mod(nparams,2)>0
+	error('Parameters must be specified as parameter/value pairs!');
+end
+
+for i=1:2:nparams
+  switch lower(varargin{i})
+    case 'delta_thresh'
+      delta_thresh=varargin{i+1};
+    case 'delta_win'
+      delta_win=varargin{i+1};
+    case 'smooth_sig'
+      smooth_sig=varargin{i+1};
+    otherwise
+  end
+end
+
+n_components=round(4*log(size(FEATURES,2))/(jl_bound_eps^2/2-jl_bound_eps^3/3));
+%n_components=400;
+RPS=kinect_gaussproj(zscore(FEATURES),n_components);
+RPS=zscore(RPS');
+
+RPS_deltas=markolab_deltacoef(RPS,delta_win); % delta coefficients, lag of 4 frames
+SCORE=sum(abs(RPS_deltas)>delta_thresh); % binarize deltas
+h=normpdf([-10:10],0,smooth_sig); % gauss smooth
+SCORE=conv(SCORE,h,'same'); % convolve
+THRESH=mean(SCORE)+.5*std(SCORE);
+idx=1:length(SCORE)-1;
+
+[~,CHANGEPOINTS]=findpeaks(SCORE,'minpeakheight',THRESH);
---
+++ kinect_analysis_proc_photometry.m
@@ -0,0 +1,45 @@
+function [PROC_DATA,PROC_TS]=kinect_analysis_proc_photometry(DATA,TS,varargin)
+%
+%
+%
+%
+%
+
+smooth_type='e';
+smooth_tau=.3;
+new_fs=100;
+detrend_win_size=3;
+dff=1;
+
+nparams=length(varargin);
+
+if mod(nparams,2)>0
+	error('Parameters must be specified as parameter/value pairs!');
+end
+
+for i=1:2:nparams
+  switch lower(varargin{i})
+    case 'smooth_type'
+      smooth_type=varargin{i+1};
+    case 'smooth_tau'
+      smooth_tau=varargin{i+1};
+    case 'new_fs'
+      new_fs=varargin{i+1};
+    case 'dff'
+      dff=varargin{i+1};
+    otherwise
+  end
+end
+
+fs=round(1./mean(diff(TS)));
+new_fs=100;
+down_fact=round(fs/new_fs);
+
+[b,a]=ellip(4,.2,40,[20]/(fs/2),'low');
+gcamp_trace_ds=downsample(filtfilt(b,a,DATA),down_fact);
+PROC_TS=downsample(TS,down_fact);
+
+gcamp_trace_smooth=markolab_smooth(gcamp_trace_ds(:),round(smooth_tau*new_fs),'r',smooth_type);
+PROC_DATA=fluolab_detrend(gcamp_trace_smooth,'fs',new_fs,'win',detrend_win_size,'dff',dff);
+PROC_DATA=PROC_DATA(new_fs*detrend_win_size:end);
+PROC_TS=PROC_TS(new_fs*detrend_win_size:end);
