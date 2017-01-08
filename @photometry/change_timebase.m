905fd89f17298314074265f329d267756699643f
jmarkow <jmarkow@cns.bu.edu>
2017-01-08 13:52:24 -0500

    Added timebase change

---
+++ @photometry/change_timebase.m
@@ -0,0 +1,17 @@
+function change_timebase(OBJ,NEW_TIMESTAMPS)
+%
+%
+%
+%
+
+% interpolate all traces using the new timestamps
+
+for i=1:length(OBJ.traces)
+	trace_types=fieldnames(OBJ.traces(i));
+	for j=1:length(trace_types)
+		new_trace=interp1(OBJ.timestamps,OBJ.traces(i).(trace_types{j}),NEW_TIMESTAMPS,'spline');
+		OBJ.traces(i).(trace_types{j})=new_trace;
+	end
+end
+OBJ.timestamps=NEW_TIMESTAMPS;
+OBJ.metadata.fs=round(mean(1./diff(NEW_TIMESTAMPS)));
--- @photometry/get_baseline.m
+++ @photometry/get_baseline.m
@@ -3,7 +3,6 @@ function get_baseline(OBJ)
 % detrends the data using a sliding window stat
 % first use vec2mat to reformat data, then apply function to each window
 
-nwin=OBJ.options.photometry.baseline_win;
 upd=photometry.proc_timer(length(OBJ.traces));
 
 for i=1:length(OBJ.traces)
@@ -11,6 +10,11 @@ for i=1:length(OBJ.traces)
 	% window the data
 
 	use_data=OBJ.traces(i).raw;
+
+	nwin=round(OBJ.options.photometry.baseline_win.*OBJ.metadata.fs);
+	if mod(nwin,2)==0
+		nwin=nwin+1;
+	end
 	padded_data=[use_data(floor(nwin/2):-1:1);use_data;use_data(end-floor(nwin/2-1):1:end)];
 	proc_mat=vec2mat(padded_data,nwin,nwin-1);
 
