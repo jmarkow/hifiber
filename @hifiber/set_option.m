066c09cb1edaaca6e923639c1544cee1fd7f4e50
jmarkow <jmarkow@cns.bu.edu>
2017-04-20 10:27:28 -0400

    Bugfix for set option

--- @hifiber/set_option.m
+++ @hifiber/set_option.m
@@ -5,7 +5,7 @@ function set_option(OBJ,OPTION,OPTION_VAL);
 %
 
 for i=1:length(OBJ)
-	if isfield(OBJ(i).options.(CATEGORY),OPTION)
+	if isfield(OBJ(i).options,OPTION)
 		use_class=class(OBJ(i).options.(OPTION));
 		if isa(OPTION_VAL,use_class)
 			OBJ(i).options.(OPTION)=OPTION_VAL;
@@ -13,6 +13,6 @@ for i=1:length(OBJ)
 			fprintf('Option %s must be %s\n',OPTION,use_class);
 		end
 	else
-		fprintf('No option %s in category %s\n',OPTION,CATEGORY);
+		fprintf('No option %s\n',OPTION);
 	end
 end
