07904e9fe2c88eaa7abefd0122429cf02a6bfa1e
Jeff Markowitz <markowitzmeister@gmail.com>
2017-05-07 18:18:44 -0400

    Rereference bugfixes

--- @hifiber/set_reference_channel.m
+++ @hifiber/set_reference_channel.m
@@ -3,10 +3,10 @@ function set_reference_channel(OBJ,CH,REF)
 %
 %
 
-nchannels=length(OBJ.traces);
 
 for i=1:length(OBJ)
+	nchannels=length(OBJ(i).traces);
 	if (CH<=nchannels & CH>0) & (REF<=nchannels & REF>=0)
-		OBJ.traces(CH).reference_channel=REF;
+		OBJ(i).traces(CH).reference_channel=REF;
 	end
 end
