c6d4741d908f324c7dbef848eefca130515a96ee
jmarkow <jmarkow@cns.bu.edu>
2017-07-01 10:31:28 -0400

    Added support for binning to change timebase

--- @hifiber/change_timebase.m
+++ @hifiber/change_timebase.m
@@ -8,22 +8,72 @@ valid_types={'dff','raw','baseline','baseline_rem','reref','reference'};
 
 % interpolate all traces using the new timestamps
 
+
 for i=1:length(OBJ)
-	for j=1:length(OBJ(i).traces)
-		trace_types=fieldnames(OBJ(i).traces(j));
-		for k=1:length(trace_types)
-			if any(strcmp(trace_types{k},valid_types)) & ~isempty(OBJ(i).traces(j).(trace_types{k}))
+	switch lower(OBJ(i).options.change_method)
 
-				nans=isnan(OBJ(i).timestamps)|isnan(OBJ(i).traces(j).(trace_types{k}));
+	case 'interp'
 
-				use_ts=OBJ(i).timestamps;
-				use_trace=OBJ(i).traces(j).(trace_types{k});
+		for j=1:length(OBJ(i).traces)
+			trace_types=fieldnames(OBJ(i).traces(j));
+			for k=1:length(trace_types)
+				if any(strcmp(trace_types{k},valid_types)) & ~isempty(OBJ(i).traces(j).(trace_types{k}))
+					nans=isnan(OBJ(i).timestamps)|isnan(OBJ(i).traces(j).(trace_types{k}));
 
-				use_ts(nans)=[];
-				use_trace(nans)=[];
+					use_ts=OBJ(i).timestamps;
+					use_trace=OBJ(i).traces(j).(trace_types{k});
 
-				new_trace=interp1(use_ts,use_trace,NEW_TIMESTAMPS);
-				OBJ(i).traces(j).(trace_types{k})=new_trace;
+					use_ts(nans)=[];
+					use_trace(nans)=[];
+
+					new_trace=interp1(use_ts,use_trace,NEW_TIMESTAMPS);
+					OBJ(i).traces(j).(trace_types{k})=new_trace;
+				end
+			end
+		end
+
+	case 'bin'
+
+		% for the new trace, assume we've been given bin idx, so just average in each bin
+		trace_types=fieldnames(OBJ(i).traces);
+
+		for j=1:length(trace_types)
+
+			if any(contains(valid_types,trace_types{j}))
+
+				concat=[OBJ(i).traces(:).(trace_types{j})];
+
+				if isempty(concat)
+					continue;
+				end
+
+				[nsamples,ntraces]=size(concat);
+
+				new_traces=nan(numel(NEW_TIMESTAMPS),ntraces);
+				[bins,~,bin_idx]=unique(NEW_TIMESTAMPS);
+				bins(isnan(bins))=[];
+
+				for k=1:length(bins)
+					if sum(NEW_TIMESTAMPS==bins(k))>1
+						new_traces(NEW_TIMESTAMPS==bins(k),:)=mean(concat(OBJ(i).timestamps==bins(k),:));
+					else
+						new_traces(NEW_TIMESTAMPS==bins(k),:)=concat(OBJ(i).timestamps==bins(k),:);
+					end
+				end
+
+				nans=find(isnan(new_traces(:,1)));
+				notnans=find(~isnan(new_traces(:,1)));
+				left_edge=min(notnans);
+				right_edge=max(notnans);
+				nans(nans<left_edge)=[];
+				nans(nans>right_edge)=[];
+				nans
+				pause();
+				new_traces(nans,:)=interp1(notnans,new_traces(notnans,:),nans,'linear');
+
+				for k=1:ntraces
+					OBJ(i).traces(k).(trace_types{j})=new_traces(:,k);
+				end
 
 			end
 		end
