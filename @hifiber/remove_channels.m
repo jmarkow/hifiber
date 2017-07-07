37b8d5732afed1936d6eb3dfdf3a6972ab05ca83
Jeff Markowitz <markowitzmeister@gmail.com>
2017-07-07 06:14:09 -0400

    Fixed remove_channels

--- @hifiber/remove_channels.m
+++ @hifiber/remove_channels.m
@@ -1,17 +1,28 @@
-function remove_channels(OBJ,NAMES)
+function remove_channels(OBJ,IDX,NAMES)
 %
 %
 %
 %
 
-to_rem=false(1,length(OBJ.metadata.channels));
+if nargin<3
+	NAMES='';
+end
 
-for i=1:length(NAMES)
-	idx=cellfun(@(x) ~isempty(x),strfind({OBJ.metadata.channels(:).name},NAMES{i}));
-	to_rem(idx)=true;
+if nargin<2 | isempty(IDX)
+	IDX=[];
+end
+
+if ~isempty(NAMES)
+	to_rem=false(1,length(OBJ.metadata.channels));
+	for i=1:length(NAMES)
+		idx=cellfun(@(x) ~isempty(x),strfind({OBJ.metadata.channels(:).name},NAMES{i}));
+		to_rem(idx)=true;
+	end
+else
+	to_rem=IDX;
 end
 
 OBJ.traces(to_rem)=[];
-OBJ.metadata.channels(to_rem)=[];
+%OBJ.metadata.channels(to_rem)=[];
 
 % to_rem
