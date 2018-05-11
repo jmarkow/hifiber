df92ae1fdc7cbae61dd8041983beadc260b3385c
wingillis <win.gillis@gmail.com>
2018-05-11 11:54:44 -0400

    save better

--- @hifiber/save_progress.m
+++ @hifiber/save_progress.m
@@ -1,12 +1,17 @@
-function save_progress(OBJ)
-%
-%
-%
-
-savefun=@(save_path,hifiber_object) save(save_path,'hifiber_object','-v7.3');
-
-if ~exist(OBJ.options.save_dir,'dir')
-	mkdir(OBJ.options.save_dir);
-end
-
-savefun(fullfile(OBJ.options.save_dir,'hifiber_object.mat'),OBJ);
+function save_progress(OBJ, fname)
+	%
+	%
+	%
+	if nargin==1
+		fname = 'hifiber_object.mat';
+	end
+	
+	savefun=@(save_path,hifiber_object) save(save_path,'hifiber_object','-v7.3');
+	
+	if ~exist(OBJ.options.save_dir,'dir')
+		mkdir(OBJ.options.save_dir);
+	end
+	
+	savefun(fullfile(OBJ.options.save_dir,fname),OBJ);
+	
+end % function
